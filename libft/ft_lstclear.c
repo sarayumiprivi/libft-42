bd97979b9dac5a6b3239f231a6164107bb1ff6c6
sarayumiprivi <152289043+sarayumiprivi@users.noreply.github.com>
2024-01-31 12:43:40 +0100

    Add files via upload

---
+++ libft/ft_lstclear.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sprivite <sprivite@student.42roma.it>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/19 15:19:38 by sprivite          #+#    #+#             */
+/*   Updated: 2024/01/30 14:08:53 by sprivite         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void*))
+{
+	t_list	*t;
+	t_list	*tmp;
+
+	if (!lst || !del)
+		return ;
+	t = *lst;
+	while (t != NULL)
+	{
+		tmp = t->next;
+		ft_lstdelone(t, del);
+		t = tmp;
+	}
+	*lst = NULL;
+}
